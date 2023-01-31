dac841f66d293ba19cd213aa03036e9e92b838b6
kopcsakpatrik <kopcsakpatrik1@gmail.com>
2023-01-31 08:57:05 +0100

    csavarlistazas mukodik

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Post, Render } from '@nestjs/common';
+import { get } from 'http';
 import { DataSource } from 'typeorm';
 import { AppService } from './app.service';
+import { Csavar } from './csavar.entity';
 
 @Controller()
 export class AppController {
@@ -13,5 +15,11 @@ export class AppController {
   @Render('index')
   index() {
     return { message: 'Welcome to the homepage' };
+  }
+
+  @Get('api/csavarbolt')
+  async listCsavar() {
+    const boltRepo = this.dataSource.getRepository(Csavar);
+    return await boltRepo.find();
   }
 }
